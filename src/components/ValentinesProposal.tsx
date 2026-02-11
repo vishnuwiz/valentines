a049acf0b19281bbe5b93756fe059aec69cbb677
Vishnu Cs <66202600+vishnuwiz@users.noreply.github.com>
2026-02-11 18:41:06 +0530

    Update ValentinesProposal.tsx

--- src/components/ValentinesProposal.tsx
+++ src/components/ValentinesProposal.tsx
@@ -132,7 +132,7 @@ export default function ValentinesProposal() {
             <h2
               className={`text-5xl font-semibold mb-8 ${playfairDisplay.className}`}
             >
-              Will you be my Valentine?
+              Will you be my Valentine Shuttuuu..?
             </h2>
             <Image
               src="/sad_hamster.png"
@@ -175,8 +175,8 @@ export default function ValentinesProposal() {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            Thank you for accepting, I love you! 💕
-            <p className="text-sm mt-4">For more information, write me!!! 💌</p>
+            Thank you Shuttttuuuuuu 😘😘, I love you! 💕
+            <p className="text-sm mt-4">Until March — take care!</p>
             <Image
               src="/hamster_jumping.gif"
               alt="Hamster Feliz"
